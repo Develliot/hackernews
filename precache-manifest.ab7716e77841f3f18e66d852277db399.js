62133477928ab4e1e7e86398273210c9b574777a
Elliot Revan <elliotrevan@gmail.com>
2020-01-13 21:04:28 +0000

    Updates

---
+++ precache-manifest.ab7716e77841f3f18e66d852277db399.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92d0b6c7efbdfd4dcaea5863e3fdfc4c",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "499b8b42c1018e99c271",
+    "url": "/hackernews/static/css/main.84d07c4b.chunk.css"
+  },
+  {
+    "revision": "ddd2db43421379bf2e7b",
+    "url": "/hackernews/static/js/2.0cc84c52.chunk.js"
+  },
+  {
+    "revision": "499b8b42c1018e99c271",
+    "url": "/hackernews/static/js/main.65c5d3b0.chunk.js"
+  },
+  {
+    "revision": "6949de6e00b9294841ce",
+    "url": "/hackernews/static/js/runtime-main.9ae721ae.js"
+  }
+]);
