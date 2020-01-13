d716d4d8867afba4f2bc5cee73f00f52882f2e97
Elliot Revan <elliotrevan@gmail.com>
2020-01-13 14:25:37 +0000

    uncomment out tests

--- src/utils/__tests__/testNumberUtils.ts
+++ src/utils/__tests__/testNumberUtils.ts
@@ -2,9 +2,9 @@ import { limitNumberWithinRange } from '../NumberUtils';
 
 describe('limitNumberWithinRange', () => {
     it('limits the number correctly correctly', () => {
-        // expect(limitNumberWithinRange(5, 1, 10)).toEqual(5);
-        // expect(limitNumberWithinRange(-5, 1, 10)).toEqual(1);
-        // expect(limitNumberWithinRange(50, 1, 10)).toEqual(10);
+        expect(limitNumberWithinRange(5, 1, 10)).toEqual(5);
+        expect(limitNumberWithinRange(-5, 1, 10)).toEqual(1);
+        expect(limitNumberWithinRange(50, 1, 10)).toEqual(10);
         expect(() => {
             limitNumberWithinRange(50, 10, 1);
         }).toThrow('min must be smaller than max');
